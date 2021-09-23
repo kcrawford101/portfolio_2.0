3f78e2199d038ae8caa461f293c53de0345c8a86
Kevin Crawford <crawfordkevin101@gmail.com>
2021-09-23 16:23:43 -0400

    added basic navbar with navbar.css

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import '../Navbar/Navbar.css';
+
+const Navbar = () => {
+    return(
+    <>
+        <div className='navbar'>            
+            <h1>Kevin Crawford's Portfolio</h1>            
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">My Work</a></li>
+                    <li><a href="assets\Kevin Crawford's Resume 2021.pdf">My Resume</a></li>
+                    <li><a href="#">Contact Me</a></li>
+                </ul>
+            </nav>            
+        </div>
+    </>
+    )
+}
+
+export default Navbar;
