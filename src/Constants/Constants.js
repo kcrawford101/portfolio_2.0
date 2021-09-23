dd94b57c0e2378952d1372f0a13fc63859cd94cb
Kevin Crawford <crawfordkevin101@gmail.com>
2021-09-23 16:32:08 -0400

    added projects and constants components

---
+++ src/Constants/Constants.js
@@ -0,0 +1,15 @@
+/* 
+Import project images here! 
+*/
+
+export const projects = [
+    {
+        title: 'Trade Guide',
+        description: "Trade Guide is a web application with primary target audience of beginner investors who need help choosing the right investment opportunities. The app uses advanced finance API providers to give the user neccessary information about available stock options and crypto options, making the decision to invest easier. It summarises the most useful information and provides access to other resourses for user to explore.",
+            image: tradeguide,
+            tags: ['jQuery, ', 'Moment.js, ', 'Javascript, ', 'Chart.js'],
+        source: 'https://github.com/ErnestAr/Trade_Guide',
+        visit: 'https://ernestar.github.io/Trade_Guide/',
+        id: 0,
+    },
+];
