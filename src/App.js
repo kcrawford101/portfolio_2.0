5442e62412f23fb12b348fa1c0761f3820f37a87
Kevin Crawford <crawfordkevin101@gmail.com>
2021-10-05 17:58:36 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-container">
+    <div className="content-wrap">
+
       <header className="App-header">
         <Navbar />        
       </header>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import '../Footer/Footer.css';
 
 const Footer = () => {
     return(
-        <>
-        
-        </>
+        <div className='main-footer'>
+            <div className='footer-content'>
+                <div className='footer-section-about'></div>
+                <div className='footer-section-links'></div>
+                <div className='footer-section-contact'></div>
+            </div>
+            
+            <div className='footer-bottom'>
+                &copy; Portfolio | Designed by Kevin Crawford
+            </div>
+        </div>                
     );
 };
 
