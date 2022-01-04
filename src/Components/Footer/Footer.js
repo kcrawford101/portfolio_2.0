003573a3f7f63f07b22bbfb7983956ccfa4163ab
Kevin Crawford <crawfordkevin101@gmail.com>
2022-01-03 21:24:19 -0500

    didn't really change anything, just removed some code

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../Footer/Footer.css';
 
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
     return(
     
     <div className='footer-bottom' id= {'contact'} >
         <div className='about-icons'>
-            <a href= 'https://www.linkedin.com/in/kcrawford117/' target='_blank'><FaLinkedin /></a>                    
+            <a className='linkedin' href= 'https://www.linkedin.com/in/kcrawford117/' target='_blank'><FaLinkedin /></a>                    
             <a href= 'https://github.com/kcrawford101' target='_blank'><FaGithub /></a>
             <br />
             <a className='email' href='mailto:crawfordkevin101@gmail.com'>crawfordkevin101@gmail.com</a>
